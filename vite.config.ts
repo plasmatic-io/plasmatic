484f2064917a04b5156a4e72b0c718987538f65b
Milos Mosovsky <milos.mosovsky@bp.com>
2023-04-10 16:27:38 +0200

    Improve dev build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
-import tsConfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
-import * as packageJson from "./package.json";
+import * as packageJson from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,20 +12,21 @@ export default defineConfig({
     react(),
     tsConfigPaths(),
     dts({
-      include: ["src"],
+      include: ['src'],
     }),
   ],
   build: {
+    emptyOutDir: false,
     lib: {
-      entry: resolve("src", "index.ts"),
-      name: "@plasmatic-ui",
-      formats: ["es", "umd", "cjs"],
+      entry: resolve('src', 'index.ts'),
+      name: '@plasmatic-ui',
+      formats: ['es', 'umd', 'cjs'],
       fileName: (format) =>
-        `plasmatic.${format == "cjs" ? "cjs" : `${format}.js`}`,
+        `plasmatic.${format == 'cjs' ? 'cjs' : `${format}.js`}`,
     },
-    minify: "esbuild",
+    minify: 'esbuild',
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
-});
+})
